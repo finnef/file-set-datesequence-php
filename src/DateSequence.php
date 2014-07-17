9eb725d93b7b9a996e1d7bb010e46ca77a9ff310
David Naughton <nihiliad@gmail.com>
2014-07-17 08:45:23 -0500

    Fixed a bug that was causing us to store only the basename, instead of an absolute path, in some cases.

--- src/DateSequence.php
+++ src/DateSequence.php
@@ -6,131 +6,129 @@ use Symfony\Component\Finder\Finder;
 
 class DateSequence extends \UmnLib\Core\Set
 {
-    protected $directory; // A directory that contains all the files in the set.
-    protected $separator = '-'; // Separates the date parts.
-    protected $suffix; // A file name suffix.
-    // Maybe TODO: Add an optional prefix?
-    protected $dateFormat = 'Y-m-d-H-i-s'; // For the PHP date() function.
-    protected $regex; // Based on all the other attributes, this becomes a predicate that members must satisfy.
-    
-    function __construct(array $params = array())
-    {
-        // TODO: Add a locking mechanism for the directory.
-        if (array_key_exists('directory', $params)) {
-            $directory = $params['directory'];
-            if (!is_dir($directory)) {
-                throw new \InvalidArgumentException("Directory '$directory' does not exist");
-            }
-            $this->directory = $directory;
-        } else {
-            $this->directory = getcwd();
-        }
+  protected $directory; // A directory that contains all the files in the set.
+  protected $separator = '-'; // Separates the date parts.
+  protected $suffix; // A file name suffix.
+  // Maybe TODO: Add an optional prefix?
+  protected $dateFormat = 'Y-m-d-H-i-s'; // For the PHP date() function.
+  protected $regex; // Based on all the other attributes, this becomes a predicate that members must satisfy.
 
-        if (array_key_exists('separator', $params)) {
-            $separator = $params['separator'];
-            if (!is_scalar($separator)) {
-                throw new \InvalidArgumentException("Param 'separator' must be a scalar");
-            }
-            $this->separator = $separator;
-        }
+  function __construct(array $params = array())
+  {
+    // TODO: Add a locking mechanism for the directory.
+    if (array_key_exists('directory', $params)) {
+      $directory = $params['directory'];
+      if (!is_dir($directory)) {
+        throw new \InvalidArgumentException("Directory '$directory' does not exist");
+      }
+      $this->directory = $directory;
+    } else {
+      $this->directory = getcwd();
+    }
 
-        if (array_key_exists('suffix', $params)) {
-            $suffix = $params['suffix'];
-            if (!is_scalar($suffix)) {
-                throw new \InvalidArgumentException("Param 'suffix' must be a scalar");
-            }
-            $this->suffix = $suffix;
-        }
+    if (array_key_exists('separator', $params)) {
+      $separator = $params['separator'];
+      if (!is_scalar($separator)) {
+        throw new \InvalidArgumentException("Param 'separator' must be a scalar");
+      }
+      $this->separator = $separator;
+    }
 
-        $this->regex = $this->generateRegex(
-            $this->separator,
-            $this->suffix
+    if (array_key_exists('suffix', $params)) {
+      $suffix = $params['suffix'];
+      if (!is_scalar($suffix)) {
+        throw new \InvalidArgumentException("Param 'suffix' must be a scalar");
+      }
+      $this->suffix = $suffix;
+    }
+
+    $this->regex = $this->generateRegex(
+      $this->separator,
+      $this->suffix
+    );
+
+    $finder = new Finder();
+    $files = $finder->name($this->regex)->in($this->directory);
+    parent::__construct();
+    foreach ($files as $file) {
+      // This is fragile! Only works to add files this way
+      // in the constructor, because we're adding files that
+      // already exist.
+      parent::add($file->getRealPath());
+    }
+  }
+
+  protected function generateRegex($separator, $suffix)
+  {
+    $dateParts = array(
+      '/^\d{4}', // year
+      '\d{2}', // month (01-12)
+      '\d{2}', // day (01-31)
+      '\d{2}', // hour (00-23)
+      '\d{2}', // minutes (00-59)
+      '\d{2}', // seconds (00-59)
+    );
+    $regex = join($dateParts, $separator);
+    if ($suffix) {
+      $suffixRegex = quotemeta($suffix);
+      $regex .= $suffixRegex;
+    }
+    $regex .= '$/';
+    return $regex;
+  }
+
+  public function add()
+  {
+    $args = func_get_args();
+    if (array_key_exists('0', $args)) {
+      $filename = $args[0];
+      if (file_exists($filename)) {
+        throw new \InvalidArgumentException("File '$filename' already exists");
+      }
+      $basename = basename($filename);
+      if (!preg_match($this->regex, $basename)) {
+        throw new \InvalidArgumentException(
+          "File '$filename' fails to satisfy predicate " . $this->regex
         );
-
-        //$f = new File_Find_Rule();
-        //$filenames = $f->name( $this->regex )->in( $this->directory );
-        $finder = new Finder();
-        $files = $finder->name($this->regex)->in($this->directory);
-        parent::__construct();
-        foreach ($files as $file) {
-            // This is fragile! Only works to add files this way
-            // in the constructor, because we're adding files that
-            // already exist.
-            parent::add($file->getFilename());
+      }
+      $dirname = dirname($filename);
+      if ($dirname == '.') {
+        $filename = $this->directory . '/' . $filename;
+      } else if ($dirname != $this->directory) {
+        throw new \InvalidArgumentException("Invalid directory '$dirname'");
+      }
+    } else {
+      unset($filename);
+      foreach (range(0,60) as $i) {
+        $filename = $this->directory . '/' . date($this->dateFormat);
+        if ($this->suffix) {
+          $filename .= $this->suffix;
         }
-    }
-
-    protected function generateRegex($separator, $suffix)
-    {
-        $dateParts = array(
-            '/^\d{4}', // year
-            '\d{2}', // month (01-12)
-            '\d{2}', // day (01-31)
-            '\d{2}', // hour (00-23)
-            '\d{2}', // minutes (00-59)
-            '\d{2}', // seconds (00-59)
-        );
-        $regex = join($dateParts, $separator);
-        if ($suffix) {
-            $suffixRegex = quotemeta($suffix);
-            $regex .= $suffixRegex;
-        }
-        $regex .= '$/';
-        return $regex;
-    }
-
-    public function add()
-    {
-        $args = func_get_args();
-        if (array_key_exists('0', $args)) {
-            $filename = $args[0];
-            if (file_exists($filename)) {
-                throw new \InvalidArgumentException("File '$filename' already exists");
-            }
-            $basename = basename($filename);
-            if (!preg_match($this->regex, $basename)) {
-                throw new \InvalidArgumentException(
-                    "File '$filename' fails to satisfy predicate " . $this->regex
-                );
-            }
-            $dirname = dirname($filename);
-            if ($dirname == '.') {
-                $filename = $this->directory . '/' . $filename;
-            } else if ($dirname != $this->directory) {
-                throw new \InvalidArgumentException("\Invalid directory '$dirname'");
-            }
+        if (file_exists($filename)) {
+          unset($filename);
+          sleep(1);
+          continue;
         } else {
-            unset($filename);
-            foreach (range(0,60) as $i) {
-                $filename = $this->directory . '/' . date($this->dateFormat);
-                if ($this->suffix) {
-                    $filename .= $this->suffix;
-                }
-                if (file_exists($filename)) {
-                    unset($filename);
-                    sleep(1);
-                    continue;
-                } else {
-                    break;
-                }
-            }
-            if (!$filename) {
-                throw new \RuntimeException("Possible race condition: cannot create unique file name");
-            }
+          break;
         }
-        touch($filename);
-        parent::add($filename);
-        return $filename;
+      }
+      if (!$filename) {
+        throw new \RuntimeException("Possible race condition: cannot create unique file name");
+      }
     }
+    touch($filename);
+    parent::add($filename);
+    return $filename;
+  }
 
-    public function delete($filename)
-    {
-        if (!file_exists($filename)) {
-            throw new \InvalidArgumentException("File '$filename' does not exist");
-        }
-        unlink($filename);
-        parent::delete($filename);
+  public function delete($filename)
+  {
+    if (!file_exists($filename)) {
+      throw new \InvalidArgumentException("File '$filename' does not exist");
     }
+    unlink($filename);
+    parent::delete($filename);
+  }
 
 /* Don't think I need this, but keeping it here for now...
     protected function sort($filenames) {
@@ -142,6 +140,5 @@ class DateSequence extends \UmnLib\Core\Set
         }
         return $sortedFileNames;
     }
-*/
-    
+ */
 }
--- tests/FileSetDateSequenceTest.php
+++ tests/FileSetDateSequenceTest.php
@@ -7,108 +7,104 @@ use Symfony\Component\Finder\Finder;
 
 class FileSetDateSequenceTest extends \PHPUnit_Framework_TestCase
 {
-    public function __construct()
-    {
+  public function testNew()
+  {
+    $emptySet = new DateSequence();
+    $this->assertInstanceOf('UmnLib\\Core\\File\\Set\\DateSequence', $emptySet);
+    $this->assertEquals(array(), $emptySet->members());
+    $this->assertEquals(0, $emptySet->size());
+
+    // $members = ...
+    $fixturesDir = dirname(__FILE__) . '/fixtures';
+    $set = new DateSequence(array(
+      'suffix' => '.xml',    
+      'directory' => $fixturesDir,
+    ));
+    $this->assertInstanceOf('UmnLib\\Core\\File\\Set\\DateSequence', $set);
+
+    $finder = new Finder();
+    $members = $finder->name('*.xml')->in($fixturesDir);
+    $membersArray = array();
+    foreach($members as $member) {
+      $membersArray[] = $member->getRealPath();
     }
+    $this->assertEquals($membersArray, $set->members());
+    $this->assertEquals(count($membersArray), $set->size());
+    return array($fixturesDir, $set);
+  }
 
-    public function testNew()
-    {
-        $emptySet = new DateSequence();
-        $this->assertInstanceOf('UmnLib\\Core\\File\\Set\\DateSequence', $emptySet);
-        $this->assertEquals(array(), $emptySet->members());
-        $this->assertEquals(0, $emptySet->size());
+  /**
+   * @depends testNew
+   */
+  public function testAdd(Array $testNewArgs)
+  {
+    list($fixturesDir, $set) = $testNewArgs;
 
-        // $members = ...
-        $fixturesDir = dirname(__FILE__) . '/fixtures';
-        $set = new DateSequence(array(
-            'suffix' => '.xml',    
-            'directory' => $fixturesDir,
-        ));
-        $this->assertInstanceOf('UmnLib\\Core\\File\\Set\\DateSequence', $set);
+    $oldSize = $set->size();
+    $filename = $set->add();
+    $newSize = $set->size();
+    $this->assertEquals($newSize, $oldSize + 1);
+    return $filename;
+  }
 
-        $finder = new Finder();
-        $members = $finder->name('*.xml')->in($fixturesDir);
-        $membersArray = array();
-        foreach($members as $member) {
-          $membersArray[] = $member->getFilename();
-        }
-        $this->assertEquals($membersArray, $set->members());
-        $this->assertEquals(count($membersArray), $set->size());
-        return array($fixturesDir, $set);
-    }
+  /**
+   * @depends testNew
+   * @depends testAdd
+   * @expectedException InvalidArgumentException
+   */
+  public function testAddException(Array $testNewArgs, $filename)
+  {
+    list($fixturesDir, $set) = $testNewArgs;
 
-    /**
-     * @depends testNew
-     */
-    public function testAdd(Array $testNewArgs)
-    {
-        list($fixturesDir, $set) = $testNewArgs;
+    // All members of a set must be unique, so can't add a file_name
+    // that already exists:
+    $set->add($filename);
 
-        $oldSize = $set->size();
-        $filename = $set->add();
-        $newSize = $set->size();
-        $this->assertEquals($newSize, $oldSize + 1);
-        return $filename;
-    }
+  }
 
-    /**
-     * @depends testNew
-     * @depends testAdd
-     * @expectedException InvalidArgumentException
-     */
-    public function testAddException(Array $testNewArgs, $filename)
-    {
-        list($fixturesDir, $set) = $testNewArgs;
+  /**
+   * @depends testNew
+   * @depends testAdd
+   * @expectedException InvalidArgumentException
+   */
+  public function testDelete(Array $testNewArgs, $filename)
+  {
+    list($fixturesDir, $set) = $testNewArgs;
 
-        // All members of a set must be unique, so can't add a file_name
-        // that already exists:
-        $set->add($filename);
+    $oldSize = $set->size();
+    $set->delete($filename);
+    $this->assertEquals($oldSize - 1, $set->size());
 
-    }
+    // Doesn't make sense to delete the same file more than once:
+    $set->delete($filename);
+  }
 
-    /**
-     * @depends testNew
-     * @depends testAdd
-     * @expectedException InvalidArgumentException
-     */
-    public function testDelete(Array $testNewArgs, $filename)
-    {
-        list($fixturesDir, $set) = $testNewArgs;
+  /**
+   * @depends testNew
+   * @expectedException InvalidArgumentException
+   */
+  public function testAddBasename(Array $testNewArgs)
+  {
+    list($fixturesDir, $set) = $testNewArgs;
 
-        $oldSize = $set->size();
-        $set->delete($filename);
-        $this->assertEquals($oldSize - 1, $set->size());
+    $basename = '2009-08-30-12-44-37.xml';
+    $filename = $set->add($basename);
+    $this->assertEquals($basename, basename($filename));
+    $this->assertEquals($fixturesDir, dirname($filename));
+    $set->delete($filename);
 
-        // Doesn't make sense to delete the same file more than once:
-        $set->delete($filename);
-    }
+    // Invalid argument because this file does not match the date format:
+    $filename = $set->add('foo');
+  }
 
-    /**
-     * @depends testNew
-     * @expectedException InvalidArgumentException
-     */
-    public function testAddBasename(Array $testNewArgs)
-    {
-        list($fixturesDir, $set) = $testNewArgs;
+  /**
+   * @depends testNew
+   * @expectedException InvalidArgumentException
+   */
+  public function testBadDirname(Array $testNewArgs)
+  {
+    list($fixturesDir, $set) = $testNewArgs;
 
-        $basename = '2009-08-30-12-44-37.xml';
-        $filename = $set->add($basename);
-        $this->assertEquals($basename, basename($filename));
-        $this->assertEquals($fixturesDir, dirname($filename));
-        $set->delete($filename);
-
-        // Invalid argument because this file does not match the date format:
-        $filename = $set->add('foo');
-    }
-
-    /**
-     * @depends testNew
-     * @expectedException InvalidArgumentException
-     */
-    public function testBadDirname(Array $testNewArgs)
-    {
-        list($fixturesDir, $set) = $testNewArgs;
-
-        $filename = $set->add('/foo/2009-08-30-12-44-37.xml');
-    }
+    $filename = $set->add('/foo/2009-08-30-12-44-37.xml');
+  }
 }
